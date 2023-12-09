f04e54cf69bcafa4c7115be2f3e0241da27bea64
Vishwanath <putchavishwanath@gmail.com>
2023-12-09 20:29:46 +0530

    Added layouts and segregated templates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,21 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Task list</title>
-</head>
+@section('title', 'List of all tasks')
 
-<body>
-   <h1>Task list</h1>
-
+@section('content')
     @forelse ($tasks as $task)
-        <li>{{ $task->title }}</li>
+        <li><a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a></li>
     @empty
         <p>No tasks found</p>
     @endforelse
-</body>
-
-</html>
+@endsection
