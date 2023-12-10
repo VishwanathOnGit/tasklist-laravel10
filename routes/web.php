f28c8c0ffee988a7def497521b91d246dc8ab6b7
Vishwanath <putchavishwanath@gmail.com>
2023-12-10 12:17:36 +0530

    Added edit module

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,12 @@ Route::get('/tasks/{id}', function ($id) {
     ]);
 })->name('tasks.show');
 
+Route::get('/tasks/{id}/edit', function ($id) {
+    return view('edit', [
+        'task' =>  Task::findOrFail($id),
+    ]);
+})->name('tasks.edit');
+
 Route::post('/tasks', function (Request $request) {
     $data = $request->validate([
         'title' => 'required|max:255',
@@ -49,3 +55,20 @@ Route::post('/tasks', function (Request $request) {
     return redirect()->route('tasks.show', ['id' => $task->id])
         ->with('success', 'Task created successfully!');
 })->name('tasks.store');
+
+Route::put('/tasks/{id}', function ($id, Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required',
+    ]);
+
+    $task = Task::findOrFail($id);
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id])
+        ->with('success', 'Task updated successfully!');
+})->name('tasks.update');
